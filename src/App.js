415590a8e67a063c17494dda5c0ea30684adffae
STEVEROD <steverodplus@gmail.com>
2021-09-27 21:41:05 -0400

    integracion de los componentes del proyecto en react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './media/Logo.png';
+import './styles/App.css';
+import siames from './media/raza siames.jpg';
+import bengala from './media/raza bengala.jpg';
+import esfinge from './media/raza esfinge.jpg';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+        <ul className="navbar">  
+            <li>
+                <img src={logo} alt="logo" className="logo"/>
+            </li>
+            <li>
+                <button className="button mainbutton">Nuevo Post</button>
+            </li>
+            <li>
+                <div className="buscar">
+                    <input placeholder="Buscar una raza"/>  
+                    <i className="fas fa-search button iconobusqueda" ></i>
+                </div>
+            </li>     
+            <li>
+                <button className="button mainbutton secondarybutton">Iniciar Sesion</button>
+            </li>
+            <li>
+            <button className ="button mainbutton" > Registro </button>
+            </li>
+        </ul>
+    </header>
+    <main>
+        <section>
+            <h1 className="navbar">Razas de Gatos</h1>
+            <ul className="contenedortarjetas">
+                <li className= "tarjeta">
+                    <div className="contenedorimagen">  
+                        <img src={siames} alt="raza siames"/>                         
+                    </div>
+                    <span className ="titulotarjeta">Siames</span>  
+                </li>               
+                <li className="tarjeta">
+                    <div  className="contenedorimagen">
+                        <img src={bengala} alt="Raza bengala"/>                        
+                    </div>
+                    <span className ="titulotarjeta">Bengala</span>                    
+                </li>               
+                <li className="tarjeta">
+                    <div className="contenedorimagen">
+                        <img src={esfinge} alt="Raza Esfinge"/>
+                    </div>
+                    <span className ="titulotarjeta">Esfinge</span> 
+                </li>               
+            </ul>  
+
+        </section>
+        <section></section>
+    </main>
+    <footer>
+
+
+    </footer>
     </div>
   );
 }
